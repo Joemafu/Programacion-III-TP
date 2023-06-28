04d08d8850172a96053db71bd69bf1e2fa327ced
Joel Mahafud <joe_mafu@hotmail.com>
2023-06-28 15:13:06 -0300

    Reestructurando clases

--- app/Utilidades/proyectocomanda.sql
+++ app/Utilidades/proyectocomanda.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-06-2023 a las 13:55:27
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 28-06-2023 a las 20:12:39
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,17 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `mesas` (
   `id` int(5) NOT NULL,
-  `estado` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `estado` varchar(50) NOT NULL,
+  `contadorClientes` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `mesas`
 --
 
-INSERT INTO `mesas` (`id`, `estado`) VALUES
-(1, 'cerrada'),
-(2, 'abierta'),
-(3, 'abierta');
+INSERT INTO `mesas` (`id`, `estado`, `contadorClientes`) VALUES
+(1, 'cerrada', 0),
+(2, 'cerrada', 0);
 
 -- --------------------------------------------------------
 
@@ -50,19 +50,46 @@ INSERT INTO `mesas` (`id`, `estado`) VALUES
 CREATE TABLE `pedidos` (
   `id` int(5) NOT NULL,
   `nombreCliente` varchar(50) NOT NULL,
-  `producto` varchar(50) NOT NULL,
   `estado` varchar(50) NOT NULL,
   `tiempoEstimado` int(3) NOT NULL,
-  `foto` varchar(75) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `foto` varchar(75) DEFAULT NULL,
+  `codigoSeguimiento` varchar(5) NOT NULL,
+  `idMesa` int(5) NOT NULL,
+  `puntuacionMesa` int(2) NOT NULL,
+  `puntuacionRestaurante` int(2) NOT NULL,
+  `puntuacionMozo` int(2) NOT NULL,
+  `puntuacionCocinero` int(2) NOT NULL,
+  `resenia` text NOT NULL,
+  `valorTotal` double NOT NULL,
+  `fecha` varchar(50) NOT NULL,
+  `entregadoATiempo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `pedidos`
 --
 
-INSERT INTO `pedidos` (`id`, `nombreCliente`, `producto`, `estado`, `tiempoEstimado`, `foto`) VALUES
-(45, 'Juan', 'milanesa con fritas', 'listo para servir', 25, '../ImagenesPedidos/2023-06-26 11-37-17hs. Juan.jpg'),
-(46, 'Pedro', 'Daiquiri', 'en preparacion', 25, '../ImagenesPedidos/2023-06-26 11-37-46hs. Pedro.jpg');
+INSERT INTO `pedidos` (`id`, `nombreCliente`, `estado`, `tiempoEstimado`, `foto`, `codigoSeguimiento`, `idMesa`, `puntuacionMesa`, `puntuacionRestaurante`, `puntuacionMozo`, `puntuacionCocinero`, `resenia`, `valorTotal`, `fecha`, `entregadoATiempo`) VALUES
+(1, 'Juan', 'listo para servir', 25, 'fotoDePedidos/fotoDeJuan', '', 0, 0, 0, 0, 0, '', 0, '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productopedidos`
+--
+
+CREATE TABLE `productopedidos` (
+  `id` int(5) NOT NULL,
+  `idPedido` int(5) NOT NULL,
+  `idProducto` int(5) NOT NULL,
+  `idEmpleado` int(5) NOT NULL,
+  `cantidad` int(5) NOT NULL,
+  `valorSubtotal` double NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `tiempoEstimado` int(5) DEFAULT NULL,
+  `entregadoATiempo` varchar(50) DEFAULT NULL,
+  `rolPreparador` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -74,21 +101,21 @@ CREATE TABLE `productos` (
   `id` int(5) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `precio` decimal(10,2) NOT NULL,
-  `tipo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `tipo` varchar(50) NOT NULL,
+  `contadorVendidos` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `precio`, `tipo`) VALUES
-(1, 'hamburguesa', '26.00', 'comida'),
-(2, 'daiquiri', '26.00', 'trago'),
-(3, 'ipa', '11.00', 'cerveza'),
-(4, 'tiramisu', '11.00', 'postre'),
-(5, 'bife', '30.99', 'comida'),
-(6, 'bife con fritas', '34.99', 'comida'),
-(7, 'milanesa con fritas', '34.99', 'comida');
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `tipo`, `contadorVendidos`) VALUES
+(1, 'hamburguesa', '26.00', 'comida', 0),
+(2, 'daiquiri', '26.00', 'trago', 0),
+(3, 'ipa', '11.00', 'cerveza', 0),
+(4, 'tiramisu', '11.00', 'postre', 0),
+(5, 'bife', '30.99', 'postre', 0),
+(6, 'milanesa con fritas', '34.99', 'comida', 0);
 
 -- --------------------------------------------------------
 
@@ -100,24 +127,24 @@ CREATE TABLE `usuarios` (
   `id` int(5) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `clave` varchar(50) NOT NULL,
-  `rol` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `rol` varchar(50) NOT NULL,
+  `suspendido` tinyint(1) NOT NULL,
+  `contadorOperaciones` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `rol`) VALUES
-(1, 'socioTest', 'socioTest', 'socio'),
-(2, 'mozoTest', 'mozoTest', 'mozo'),
-(3, 'bartenderTest', 'bartenderTest', 'bartender'),
-(4, 'cerveceroTest', 'cerveceroTest', 'cervecero'),
-(5, 'cocineroTest', 'cocineroTest', 'cocinero'),
-(6, 'altaTest', 'pa$$', 'mozo'),
-(8, 'altaTest2', 'pa$$', 'mozo'),
-(9, 'altaTest3', 'pa$$', 'cervecero'),
-(10, 'altaCSVTest1', 'claveTest', 'bartender'),
-(11, 'altaCSVTest2', 'claveTest', 'bartender');
+INSERT INTO `usuarios` (`id`, `usuario`, `clave`, `rol`, `suspendido`, `contadorOperaciones`) VALUES
+(1, 'socioTest', 'socioTest', 'socio', 0, 0),
+(2, 'mozoTest', 'mozoTest', 'mozo', 0, 0),
+(3, 'bartenderTest', 'bartenderTest', 'bartender', 0, 0),
+(4, 'cerveceroTest', 'cerveceroTest', 'cervecero', 0, 0),
+(5, 'cocineroTest', 'cocineroTest', 'cocinero', 0, 0),
+(6, 'altaTest', 'pa$$', 'mozo', 0, 0),
+(7, 'altaTest5', 'pa$$word', 'mozo', 0, 0),
+(8, 'altaTest6', 'altaTest6', 'bartender', 0, 0);
 
 --
 -- Índices para tablas volcadas
@@ -133,6 +160,12 @@ ALTER TABLE `mesas`
 -- Indices de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productopedidos`
+--
+ALTER TABLE `productopedidos`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -155,25 +188,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `mesas`
 --
 ALTER TABLE `mesas`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `productopedidos`
+--
+ALTER TABLE `productopedidos`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
